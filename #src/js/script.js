6c0d91d5689d6842edcbb0bae66cc190396fbdb3
Daniir Artykov <artykovdaniyar@gmail.com>
2022-03-08 13:08:22 +0600

    writing README.md and setting layout styles

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,36 +1,33 @@
-import $ from "jquery";
-import "magnific-popup";
-
-window.addEventListener("DOMContentLoaded", () => {
-	if (window.innerWidth > 400) {
-		$(".portfolio-gallery").magnificPopup({
-			delegate: "a",
-			type: "image",
-			tLoading: "Loading image #%curr%...",
-			mainClass: "mfp-img-mobile",
-			gallery: {
-				enabled: true,
-				navigateByImgClick: true,
-				preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
-				arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>',
-				closeMarkup: '<button title="%title%" type="button" class="mfp-close">Close</button>',
-			},
-		});
-	} else if (window.innerWidth < 400) {
-		const images = document.querySelectorAll(".portfolio-gallery");
-		images.forEach((img) => {
-			img.addEventListener("click", (e) => {
-				e.preventDefault();
-			});
-		});
-	}
-});
-
 const overlay = document.querySelector(".overlay");
 const popupWindow = document.querySelector(".popup-window");
 const contactMeBtn = document.querySelector(".hero-info__btn");
 const poppWindowCloseBtn = popupWindow.querySelector(".popup-window__btn");
 const divWraper = document.querySelector(".wrapper");
+import $ from "jquery";
+import "magnific-popup";
+
+if (window.innerWidth > 400) {
+	$(".portfolio-gallery").magnificPopup({
+		delegate: "a",
+		type: "image",
+		tLoading: "Loading image #%curr%...",
+		mainClass: "mfp-img-mobile",
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+			arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>',
+			closeMarkup: '<button title="%title%" type="button" class="mfp-close">Close</button>',
+		},
+	});
+} else if (window.innerWidth < 400) {
+	const images = document.querySelectorAll(".portfolio-gallery");
+	images.forEach((img) => {
+		img.addEventListener("click", (e) => {
+			e.preventDefault();
+		});
+	});
+}
 
 function openPopupHandler() {
 	popupWindow.classList.add("active");
